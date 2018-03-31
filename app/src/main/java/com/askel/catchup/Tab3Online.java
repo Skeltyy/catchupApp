fcbf76315946d84edb09581e0fd940ddac73afb1
Askelton <askelton744@googlemail.com>
2018-03-31 15:09:15 +0100

    initial commit

---
+++ app/src/main/java/com/askel/catchup/Tab3Online.java
@@ -0,0 +1,16 @@
+package com.askel.catchup;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Online extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3online, container, false);
+        return rootView;
+    }
+}
