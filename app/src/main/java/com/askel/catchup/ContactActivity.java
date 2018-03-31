2fbc6a00909e0510b294ce16ab4a40129d51c5d2
Conor O'Donnell <conor.odonnell@sita.aero>
2018-03-31 18:19:26 +0100

    Liked settings button to activity.

--- app/src/main/java/com/askel/catchup/ContactActivity.java
+++ app/src/main/java/com/askel/catchup/ContactActivity.java
@@ -1,5 +1,6 @@
 package com.askel.catchup;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -78,6 +79,7 @@ public class ContactActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(ContactActivity.this, SettingsActivity.class));
             return true;
         }
 
